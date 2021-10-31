91785b2db6c02a85d59e7bb17a78c5b1abc336aa
Anjana <66191342+anj-ana@users.noreply.github.com>
2021-10-31 06:03:40 -0700

    override-java project

---
+++ Override.java
@@ -0,0 +1,28 @@
+package mypack;
+
+
+
+class Breakup{
+	void reason() {
+		System.out.println("Happy Birthday");
+	}
+}
+class Birthday extends Breakup{
+	@Override
+	void reason(String call) {
+		System.out.println("Good days");
+	}
+}
+
+public class Override {
+
+
+
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
