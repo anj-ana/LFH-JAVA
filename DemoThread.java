1afe1463ac4b46f609cf22b52dac6cd05f65c015
Anjana <66191342+anj-ana@users.noreply.github.com>
2021-12-10 09:39:17 -0800

    Thread

---
+++ DemoThread.java
@@ -0,0 +1,60 @@
+package mypack;
+
+class ThreadOne extends Thread{
+	public void run() {
+	for(int i=0;i<6;i++) {
+		System.out.println("Hello!Anjana here!!");
+	}
+	try {
+		Thread.sleep(1000);
+		
+	}
+	catch(Exception e) {
+		
+	}
+}
+}
+class ThreadTwo extends Thread{
+	public void run() {
+		for(int i=0;i<5;i++) {
+			System.out.println("Heyy!what a surprise");
+		}
+		try {
+			Thread.sleep(2000);
+			
+		}
+		catch(Exception e) {
+			
+		}
+}
+}
+class ThreadThree extends Thread{
+	public void run() {
+		for(int i=0;i<4;i++) {
+			System.out.println("What about having a coffee?!!");
+		}
+		try {
+			Thread.sleep(1000);
+			
+		}
+		catch(Exception e) {
+			
+}
+	}
+}
+
+public class DemoThread {
+
+	public static void main(String[] args)throws InterruptedException {
+		
+		ThreadOne obj1 = new ThreadOne();
+		ThreadTwo obj2 = new ThreadTwo();
+		ThreadThree obj3 = new ThreadThree();
+		obj1.start();
+		obj2.start();
+		obj3.start();
+
+	}
+	
+
+}
