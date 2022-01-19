dd009636f6fc2feec11a8665a86d6d5da724a13f
Anjana <66191342+anj-ana@users.noreply.github.com>
2022-01-20 00:33:49 +0530

    Name changed

--- HELLO WORLD/src/mypack/Task.java
+++ HELLO WORLD/src/mypack/Task.java
@@ -4,7 +4,7 @@ public class Task {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-         System.out.println("Hello Anjana");
+         System.out.println("Hello Anju");
 	}
 
 }
